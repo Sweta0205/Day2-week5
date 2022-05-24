25938092ed596a125980130539e80ff73cdd7eab
sweta sriramadasu <swethasriramdas2311@gmail.com>
2022-05-24 17:03:25 +0200

    commit

---
+++ selector.js
@@ -0,0 +1,9 @@
+function changeHeading()
+{
+    var heading1=document.getElementsByClassName("blog-header-logo text-dark").innerHtml="changed"
+}
+function changeBgColor()
+{
+document.getElementsByClassName('jumbotron p-4 p-md-5 rounded').style.backgroundColor = "pink";
+
+}
